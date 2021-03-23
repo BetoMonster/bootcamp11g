c636887fea5419462d4e9a568127cbb81ea4fffa
BetoMonster <betomonster@gmail.com>
2021-03-22 22:27:36 -0600

    session 9

---
+++ s9javascript/js/main.js
@@ -0,0 +1,17 @@
+//alert("Hola Koders");
+console.log("Hola Koders");
+
+var number1=10;
+var number2=5;
+
+console.log( number1+number2);
+console.log( number1-number2);
+console.log( number1*number2);
+console.log( number1/number2);
+console.log( number1%number2);
+console.log( '11'+number1 );
+console.log( '11'-number2 );
+console.warn(++number1, ++number2);
+console.error(--number1, --number2);
+result= ++number1;
+document.write('Hola Koders'+ result );
